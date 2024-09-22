f6aa7ed15be56c6d40236c836de7e86c2cb32ca9
Abinadabe Oliveira <abinadabedev@outlook.com>
2024-09-22 19:25:57 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao Me Indica Aê!");
+        System.out.println("Filme: Interestelar");
+
+        int ano = 2022;
+        System.out.println("Ano de Lançamento: " + ano);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.4;
+        System.out.println("Nota do Filme: " + notaDoFilme);
+
+    }
+}
